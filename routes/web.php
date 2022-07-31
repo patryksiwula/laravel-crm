e8abf45ab54b53f2298276a003b6181c5dcc50f5
Thrillerek <patryk.siw@gmail.com>
2022-07-31 19:18:02 +0200

    Add redirect from home to dashboard

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::redirect('/', 'dashboard');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
