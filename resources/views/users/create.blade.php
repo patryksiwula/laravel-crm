0f9a33ed7132231c27eaa297aee332fd37f48657
Patryk Siwula <patryk.siw@gmail.com>
2022-08-02 01:40:37 +0200

    Add password field to create user form

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,32 +8,50 @@
 
         <div class="w-full h-full px-80 py-10 bg-gray-200">
             <div class="w-full bg-white p-8 shadow-md">
-                <form action="#" method="POST">
+                <form action="{{ route('users.store') }}" method="POST">
                     @csrf
 
                     <div class="block">
-                        <label for="user_name" class="font-bold text-base text-black block mb-3">
+                        <label for="name" class="font-bold text-base text-black block mb-3">
                             {{ __('Name') }}
                         </label>
-                        <input type="text" placeholder="{{ __('User name') }}" name="user_name" class="w-full border-[1.5px] border-form-stroke
+                        <input type="text" placeholder="{{ __('User name') }}" name="name" class="w-full border-[1.5px] border-form-stroke
                             rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
                             active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
                     </div>
 
                     <div class="mt-8">
-                        <label for="user_email" class="font-bold text-base text-black block mb-3">
+                        <label for="email" class="font-bold text-base text-black block mb-3">
                             {{ __('Email') }}
                         </label>
-                        <input type="email" placeholder="{{ __('User email') }}" name="user_email" class="w-full border-[1.5px] border-form-stroke
+                        <input type="email" placeholder="{{ __('User email') }}" name="email" class="w-full border-[1.5px] border-form-stroke
+                            rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
+                            active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
+                    </div>
+
+					<div class="mt-8">
+                        <label for="password" class="font-bold text-base text-black block mb-3">
+                            {{ __('Password') }}
+                        </label>
+                        <input type="password" name="password" class="w-full border-[1.5px] border-form-stroke
+                            rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
+                            active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
+                    </div>
+
+					<div class="mt-8">
+                        <label for="password_confirmation" class="font-bold text-base text-black block mb-3">
+                            {{ __('Confirm password') }}
+                        </label>
+                        <input type="password" name="password_confirmation" class="w-full border-[1.5px] border-form-stroke
                             rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
                             active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
                     </div>
 
                     <div class="mt-8">
-                        <label for="user_role" class="font-bold text-base text-black block mb-3">
+                        <label for="role" class="font-bold text-base text-black block mb-3">
                             {{ __('Role') }}
                         </label>
-                        <select class="w-full border-[1.5px] border-form-stroke rounded-lg py-3 px-5 font-medium text-body-color
+                        <select name="role" class="w-full border-[1.5px] border-form-stroke rounded-lg py-3 px-5 font-medium text-body-color
                             outline-none focus:border-primary active:border-primary transition disabled:bg-[#F5F7FD]
                             disabled:cursor-default appearance-none">
                             
