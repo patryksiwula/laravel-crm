b75750dba2e925dfdc565729a7dfc3c187e9de8c
Patryk Siwula <patryk.siw@gmail.com>
2022-08-02 13:21:38 +0200

    Add organization factory and seeder

---
+++ database/factories/Client/OrganizationFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories\Client;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class OrganizationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->company(),
+			'email' => fake()->email(),
+			'phone' => fake()->e164PhoneNumber(),
+			'address' => fake()->address(),
+			'vat' => fake()->numerify('##########')
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Client\Organization;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Organization::factory(60)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
