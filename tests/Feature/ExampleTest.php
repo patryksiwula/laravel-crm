4cbea1154c55d82bd48084af2a4c33c396f07113
Patryk Siwula <patryk.siw@gmail.com>
2022-08-11 19:45:25 +0200

    Add test cases for homepage redirecting

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,27 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
+    public function testGuestIsBeingRedirectedToLoginPage(): void
     {
         $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $response->assertRedirect('/login');
     }
+
+	public function testUserIsBeingRedirectedToDashboard(): void
+	{
+		/**
+		 * @var \Illuminate\Contracts\Auth\Authenticatable
+		 */
+		$user = User::factory()->create();
+
+		$this->actingAs($user);
+		$response = $this->get('/');
+        $response->assertRedirect('/dashboard');
+	}
 }
