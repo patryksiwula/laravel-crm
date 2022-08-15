ef793dd12110a802cb34822f2b9e5d055760c495
Patryk Siwula <patryk.siw@gmail.com>
2022-08-15 17:48:58 +0200

    Add fillable array for organisation model

--- app/Models/Client/Organization.php
+++ app/Models/Client/Organization.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Organization extends Model
 {
     use HasFactory;
+
+	protected $fillable = [
+		'name',
+		'email',
+		'phone',
+		'address',
+		'vat'
+	];
 }
