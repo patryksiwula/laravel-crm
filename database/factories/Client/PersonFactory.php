4992020576073bab315b9fc45503f47141ac2ee6
Patryk Siwula <patryk.siw@gmail.com>
2022-08-15 17:48:23 +0200

    Add person factory

---
+++ database/factories/Client/PersonFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories\Client;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class PersonFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+			'email' => fake()->unique()->safeEmail(),
+			'phone' => fake()->unique()->e164PhoneNumber(),
+			'address' => fake()->address()
+        ];
+    }
+}
