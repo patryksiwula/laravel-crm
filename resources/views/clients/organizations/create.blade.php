16f195dec3e335369a0c2b77e645d6c742d76ff6
Patryk Siwula <patryk.siw@gmail.com>
2022-08-17 19:32:52 +0200

    Add create pages for people and organisations

---
+++ resources/views/clients/organizations/create.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="min-h-screen w-full ml-60 bg-gray-200">
+        <div class="w-full py-5 text-center bg-white shadow-sm">
+            <h1 class="text-xl font-bold">{{ __('Create new organisation') }}</h1>
+        </div>
+
+        <div class="w-full h-full px-80 py-10 bg-gray-200">
+            <div class="w-full bg-white p-8 shadow-md">
+                <form action="{{ route('organizations.store') }}" method="POST">
+                    @csrf
+
+                    <div class="block">
+                        <label for="name" class="font-bold text-base text-black block mb-3">
+                            {{ __('Name') }}
+                        </label>
+                        <input type="text" placeholder="{{ __('Organisation name') }}" name="name" class="w-full border-[1.5px] border-form-stroke
+                            rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
+                            active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
+                    </div>
+
+                    <div class="mt-8">
+                        <label for="email" class="font-bold text-base text-black block mb-3">
+                            {{ __('Email') }}
+                        </label>
+                        <input type="email" placeholder="{{ __('Organisation email') }}" name="email" class="w-full border-[1.5px] border-form-stroke
+                            rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
+                            active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
+                    </div>
+
+					<div class="mt-8">
+                        <label for="phone" class="font-bold text-base text-black block mb-3">
+                            {{ __('Phone') }}
+                        </label>
+                        <input type="text" name="phone" placeholder="{{ __('Phone number') }}" class="w-full border-[1.5px] border-form-stroke
+                            rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
+                            active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
+                    </div>
+
+					<div class="mt-8">
+                        <label for="address" class="font-bold text-base text-black block mb-3">
+                            {{ __('Address') }}
+                        </label>
+                        <input type="text" name="address" placeholder="{{ __('Organisation address') }}" class="w-full border-[1.5px] border-form-stroke
+                            rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
+                            active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
+                    </div>
+
+                    <div class="mt-8">
+                        <label for="vat" class="font-bold text-base text-black block mb-3">
+                            {{ __('VAT number') }}
+                        </label>
+                        <input type="text" name="vat" placeholder="{{ __('Organisation VAT number') }}" class="w-full border-[1.5px] border-form-stroke
+                            rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
+                            active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
+                    </div>
+
+                    <input type="submit" class="mt-10 text-lg bg-green-500 hover:bg-green-700 text-white py-2 px-6 rounded
+                    focus:outline-none focus:shadow-outline font-bold cursor-pointer">
+                </form>
+            </div>
+        </div>
+    </main>
+@endsection
