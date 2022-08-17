5866191ec7b7d7a3b64b9db67fbe2b57f1560168
Patryk Siwula <patryk.siw@gmail.com>
2022-08-17 19:54:23 +0200

    Fix active menu item bug for people routes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,13 +17,13 @@
                 </svg>
                 <span class="font-semibold text-sm">{{ __('Users') }}</span>
             </x-nav-link>
-            <x-nav-link :href="route('organizations.index')" :active="request()->is('organizations*') || request()->is('people')">
+            <x-nav-link :href="route('organizations.index')" :active="request()->is('organizations*') || request()->is('people*')">
                 <svg viewBox="0 0 24 25" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" stroke="none">
                     <path d="m19 4h-4v-1a3 3 0 0 0 -6 0v1h-4a5.006 5.006 0 0 0 -5 5v10a5.006 5.006 0 0 0 5 5h14a5.006 5.006 0 0 0 5-5v-10a5.006 5.006 0 0 0 -5-5zm-8-1a1 1 0 0 1 2 0v2a1 1 0 0 1 -2 0zm11 16a3 3 0 0 1 -3 3h-14a3 3 0 0 1 -3-3v-10a3 3 0 0 1 3-3h4.184a2.982 2.982 0 0 0 5.632 0h4.184a3 3 0 0 1 3 3zm-12-9h-5a1 1 0 0 0 -1 1v8a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1v-8a1 1 0 0 0 -1-1zm-1 8h-3v-6h3zm11-3a1 1 0 0 1 -1 1h-5a1 1 0 0 1 0-2h5a1 1 0 0 1 1 1zm0-4a1 1 0 0 1 -1 1h-5a1 1 0 0 1 0-2h5a1 1 0 0 1 1 1zm-2 8a1 1 0 0 1 -1 1h-3a1 1 0 0 1 0-2h3a1 1 0 0 1 1 1z"/>
                 </svg>
                 <span class="font-semibold text-sm">{{ __('Clients') }}</span>
             </x-nav-link>
-			<ul @class(['hidden' => !(request()->is('organizations*') || request()->is('people'))])>
+			<ul @class(['hidden' => !(request()->is('organizations*') || request()->is('people*'))])>
 				<x-nav-link-child :href="route('organizations.index')" :active="request()->is('organizations*')">
 					<svg viewBox="0 0 24 25" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" stroke="none">
 						<path d="m19 4h-4v-1a3 3 0 0 0 -6 0v1h-4a5.006 5.006 0 0 0 -5 5v10a5.006 5.006 0 0 0 5 5h14a5.006 5.006 0 0 0 5-5v-10a5.006 5.006 0 0 0 -5-5zm-8-1a1 1 0 0 1 2 0v2a1 1 0 0 1 -2 0zm11 16a3 3 0 0 1 -3 3h-14a3 3 0 0 1 -3-3v-10a3 3 0 0 1 3-3h4.184a2.982 2.982 0 0 0 5.632 0h4.184a3 3 0 0 1 3 3zm-12-9h-5a1 1 0 0 0 -1 1v8a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1v-8a1 1 0 0 0 -1-1zm-1 8h-3v-6h3zm11-3a1 1 0 0 1 -1 1h-5a1 1 0 0 1 0-2h5a1 1 0 0 1 1 1zm0-4a1 1 0 0 1 -1 1h-5a1 1 0 0 1 0-2h5a1 1 0 0 1 1 1zm-2 8a1 1 0 0 1 -1 1h-3a1 1 0 0 1 0-2h3a1 1 0 0 1 1 1z"/>
