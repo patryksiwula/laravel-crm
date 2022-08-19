a0e6a35e4e78e53742137e5c2b12a8f67d53a50d
Patryk Siwula <patryk.siw@gmail.com>
2022-08-19 17:29:47 +0200

    Add page for permission edit

---
+++ resources/views/users/permissions/edit.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="min-h-screen w-full ml-60 bg-gray-200">
+        <div class="w-full py-5 text-center bg-white shadow-sm">
+            <h1 class="text-xl font-bold">{{ __('Editing permission') }}</h1>
+        </div>
+
+        <div class="w-full h-full px-80 py-10 bg-gray-200">
+            <div class="w-full bg-white p-8 shadow-md">
+                <form action="{{ route('permissions.update', compact('permission')) }}" method="POST">
+                    @csrf
+					@method('PATCH')
+
+                    <div class="block">
+                        <label for="name" class="font-bold text-base text-black block mb-3">
+                            {{ __('Name') }}
+                        </label>
+                        <input type="text" placeholder="{{ __('Permission name') }}" name="name" value="{{ $permission->name }}" class="w-full border-[1.5px] border-form-stroke
+                            rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
+                            active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
+                    </div>
+
+                    <input type="submit" class="mt-10 text-lg bg-green-500 hover:bg-green-700 text-white py-2 px-6 rounded
+                    focus:outline-none focus:shadow-outline font-bold cursor-pointer">
+                </form>
+            </div>
+        </div>
+    </main>
+@endsection
