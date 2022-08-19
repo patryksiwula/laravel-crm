19b16c50fdd7e8c953ccd083a661ca2c2c76a3a3
Patryk Siwula <patryk.siw@gmail.com>
2022-08-19 17:30:02 +0200

    Remove show method

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -60,17 +60,6 @@ class RoleController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  \Spatie\Permission\Models\Role  $role
