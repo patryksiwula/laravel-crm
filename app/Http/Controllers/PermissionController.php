d8c1ac1d09516fc5dac87c18c8476c6f6d8d99d4
Patryk Siwula <patryk.siw@gmail.com>
2022-08-19 17:36:35 +0200

    Add permission controller

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePermissionRequest;
+use App\Http\Requests\UpdatePermissionRequest;
+use App\Services\PermissionService;
+use Spatie\Permission\Models\Permission;
+
+class PermissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $permissions = Permission::with(['roles' => function ($query) {
+			$query->select('name');
+		}])->get();
+
+		return view('users.permissions.list', compact('permissions'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $this->authorize('create-users');
+
+		return view('users.permissions.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePermissionRequest  $request
+	 * @param  \App\Services\PermissionService  $permissionService
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePermissionRequest $request, PermissionService $permissionService)
+    {
+        $this->authorize('create-users');
+        $permissionService->createPermission($request->validated('name'));
+
+		return redirect()->route('permissions.index')
+			->with('action', 'permission_created');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \Spatie\Permission\Models\Permission  $permission
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Permission $permission)
+    {
+        $this->authorize('edit-users');
+
+		return view('users.permissions.edit', compact('permission'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePermissionRequest  $request
+     * @param  \Spatie\Permission\Models\Permission  $permission
+	 * @param  \App\Services\PermissionService  $permissionService
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePermissionRequest $request, Permission $permission, PermissionService $permissionService)
+    {
+        $this->authorize('edit-users');
+
+        $permissionService->updatePermission(
+			$permission,
+			$request->validated('name'),
+		);
+
+		return redirect()->route('permissions.index')
+			->with('action', 'permission_updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Spatie\Permission\Models\Permission  $Permission
+	 * @param  \App\Services\PermissionService $permissionService
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Permission $permission, PermissionService $permissionService)
+    {
+        $this->authorize('delete-users');
+		$permissionService->deletePermission($permission);
+
+		return redirect()->route('permissions.index')
+			->with('action', 'permission_deleted');
+    }
+}
