74b5d5c1b125a41e3fa8fba22331a03a37cbeb1c
Patryk Siwula <patryk.siw@gmail.com>
2022-08-24 16:54:53 +0200

    Add product controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductRequest;
+use App\Models\Product;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function index(): View
+    {
+        $products = Product::all();
+
+		return view('products.list', compact('products'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\ProductRequest  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(ProductRequest $request): RedirectResponse
+    {
+		$this->authorize('create-products');
+        Product::create($request->validated());
+
+		return redirect()->route('products.index')
+			->with('action', 'product_created');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function edit(Product $product): View
+    {
+        $this->authorize('edit-products');
+
+		return view('products.edit', compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\ProductRequest  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(ProductRequest $request, Product $product): RedirectResponse
+    {
+        $this->authorize('edit-products');
+		$product->update($request->validated());
+
+		return redirect()->route('products.index')
+			->with('action', 'product_updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy(Product $product): RedirectResponse
+    {
+        $this->authorize('delete-products');
+		$product->delete();
+
+		return redirect()->route('products.index')
+			->with('action', 'product_deleted');
+    }
+}
