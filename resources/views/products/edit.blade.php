ac8f66ab1feda4e8da3413ffd868e0e3afb3aec6
Patryk Siwula <patryk.siw@gmail.com>
2022-08-24 16:42:55 +0200

    Add pages for products

---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="min-h-screen w-full ml-60 bg-gray-200">
+        <div class="w-full py-5 text-center bg-white shadow-sm">
+            <h1 class="text-xl font-bold">{{ __('Editing product') }}</h1>
+        </div>
+
+        <div class="w-full h-full px-80 py-10 bg-gray-200">
+            <div class="w-full bg-white p-8 shadow-md">
+                <form action="{{ route('products.update', compact('product')) }}" method="POST">
+                    @csrf
+					@method('PATCH')
+
+                    <div class="block">
+                        <label for="name" class="font-bold text-base text-black block mb-3">
+                            {{ __('Name') }}
+                        </label>
+                        <input type="text" placeholder="{{ __('Product name') }}" name="name" value="{{ $product->name }}" class="w-full border-[1.5px] border-form-stroke
+                            rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
+                            active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
+                    </div>
+
+                    <div class="mt-8">
+                        <label for="description" class="font-bold text-base text-black block mb-3">
+                            {{ __('Description') }}
+                        </label>
+                        <textarea placeholder="{{ __('Product description') }}" name="description" class="w-full border-[1.5px] border-form-stroke
+                            rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
+                            active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
+							{{ $product->description }}
+						</textarea>
+                    </div>
+
+					<div class="mt-8">
+						<label for="price" class="font-bold text-base text-black block mb-3">
+                            {{ __('Price') }}
+                        </label>
+                        <input type="number" placeholder="{{ __('Price per unit') }}" name="price" value="{{ $product->price }}" class="w-full border-[1.5px] border-form-stroke
+                            rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
+                            active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
+					</div>
+
+					<div class="mt-8">
+						<label for="quantity" class="font-bold text-base text-black block mb-3">
+                            {{ __('Quantity') }}
+                        </label>
+                        <input type="number" placeholder="{{ __('Product quantity') }}" name="quantity" value="{{ $product->quantity }}" class="w-full border-[1.5px] border-form-stroke
+                            rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
+                            active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
+					</div>
+
+                    <input type="submit" class="mt-10 text-lg bg-green-500 hover:bg-green-700 text-white py-2 px-6 rounded
+                    focus:outline-none focus:shadow-outline font-bold cursor-pointer">
+                </form>
+            </div>
+        </div>
+    </main>
+@endsection
---
+++ resources/views/products/list.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="min-h-screen w-full ml-60 bg-gray-200">
+        <div class="w-full py-5 text-center bg-white shadow-sm">
+            <h1 class="text-xl font-bold">{{ __('Products') }}</h1>
+        </div>
+
+        <div class="flex flex-col w-full h-full px-10 pt-12 pb-10 bg-gray-200">
+			@can('create-products')
+				<div class="mb-6">
+					<a href="{{ route('products.create') }}" class="text-xl bg-green-500 hover:bg-green-700 text-white py-3 px-6 rounded
+						focus:outline-none focus:shadow-outline font-bold">
+						{{ __('Add product') }} +
+					</a>
+				</div>
+			@endcan
+			
+            <div class="w-full bg-white">
+                <div class="text-gray-900 bg-gray-200">
+                    <div class="flex justify-center">
+                        <table class="w-full text-md bg-white shadow-md rounded mb-4">
+                            <tbody>
+                                <tr class="border-b">
+									<th class="text-left p-3 px-5">{{ __('ID') }}</th>
+                                    <th class="text-left p-3 px-5">{{ __('Name') }}</th>
+									<th class="text-left p-3 px-5">{{ __('Price per unit') }}</th>
+                                    <th class="text-left p-3 px-5">{{ __('Quantity') }}</th>
+
+									@canany(['edit-products', 'delete-products'])
+										<th class="text-left p-3 px-5">{{ __('Action') }}</th>
+									@endcanany
+                                </tr>
+                                
+                                @foreach ($products as $product)
+                                    <tr class="border-b hover:bg-orange-100 bg-gray-100">
+                                        <td class="p-3 px-5">{{ $product->id }}</td>
+										<td class="p-3 px-5">{{ $product->name }}</td>
+										<td class="p-3 px-5">{{ $product->price }}</td>
+										<td class="p-3 px-5">{{ $product->quantity }}</td>
+
+										@canany(['edit-products', 'delete-products'])
+											<td class="p-3 px-5 flex">
+												@can('edit-products')
+													<a href="{{ route('products.edit', compact('product')) }}" class="text-sm bg-blue-500 hover:bg-blue-700 
+														text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+														{{ __('Edit') }}
+													</a>
+												@endcan
+												
+												@can('delete-products')
+													<form action="{{ route('products.destroy', compact('product')) }}" method="POST">
+														@csrf
+														@method('DELETE')
+
+														<button class="ml-1 text-sm bg-red-500 hover:bg-red-700 text-white 
+															py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+															{{ __('Delete') }}
+														</button>
+													</form>
+												@endcan
+											</td>
+										@endcanany
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
