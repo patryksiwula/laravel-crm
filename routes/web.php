636736e968e47d98db6f3e269419327c216f6749
Patryk Siwula <patryk.siw@gmail.com>
2022-08-26 02:40:10 +0200

    Add routes for invoice

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
+use App\Http\Livewire\DynamicInvoice;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,17 +28,17 @@ Route::middleware(['auth'])->group(function () {
 
     Route::redirect('/', 'dashboard');
 	
-    Route::resource('users', UserController::class);
+    Route::resource('users', UserController::class)->except('show');
 	Route::resource('roles', RoleController::class)->except('show');
 	Route::resource('permissions', PermissionController::class)->except('show');
 	Route::resource('organizations', OrganizationController::class)->except('show');
 	Route::resource('people', PersonController::class)->except('show');
 
-	Route::view('products/create', 'products.create')->name('products.create');
+	Route::view('/products/create', 'products.create')->name('products.create');
 	Route::resource('products', ProductController::class)->except(['show', 'create']);
 	
-	Route::view('invoices/create', 'invoices.create')->name('invoices.create');
-	Route::resource('invoices', InvoiceController::class)->except(['show', 'create']);
+	Route::view('/invoices/create', 'invoices.create')->name('invoices.create');
+	Route::resource('invoices', InvoiceController::class)->except(['create', 'store']);
 });
 
 require __DIR__ . '/auth.php';
