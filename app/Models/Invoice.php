063e69d881bb7af7dde18f0626f8066058ed0e62
Patryk Siwula <patryk.siw@gmail.com>
2022-08-29 02:11:32 +0200

    Add syncItems method

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Support\Collection;
+use PhpParser\ErrorHandler\Collecting;
 
 class Invoice extends Model
 {
@@ -38,6 +40,14 @@ class Invoice extends Model
 		return $this->belongsToMany(Product::class)
 			->withPivot('quantity')
 			->withTimestamps();
+	}
+
+	public function syncItems(Collection $invoiceItems): void
+	{
+		$this->products()->detach();
+
+		foreach ($invoiceItems as $item)
+			$this->products()->attach(Product::find($item['product_id']), ['quantity' => $item['quantity']]);
 	}
 
 	public function addItem(Product $product, int $quantity): void
