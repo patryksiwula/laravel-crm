aca55e170d3232685bcc13b743010ffe96f1bc31
Patryk Siwula <patryk.siw@gmail.com>
2022-08-29 02:10:21 +0200

    Add edit template for invoices

---
+++ resources/views/invoices/edit.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="min-h-screen w-full ml-60 bg-gray-200">
+        <div class="w-full py-5 text-center bg-white shadow-sm">
+            <h1 class="text-xl font-bold">{{ __('Editing invoice') }}</h1>
+        </div>
+
+		<div class="w-full h-full px-80 py-10 bg-gray-200">
+            <div class="w-full bg-white p-8 shadow-md">
+				<h2 class="text-2xl font-semibold">{{ __('Invoice details') }}</h2>
+
+				<livewire:dynamic-invoice-update :invoiceId="$invoiceId"/>
+			</div>
+			
+        </div>
+	</main>
+@endsection
