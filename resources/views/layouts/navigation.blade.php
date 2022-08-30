b27593a41f3e21030cd4b53f92a9fe476b785ac6
Patryk Siwula <patryk.siw@gmail.com>
2022-08-30 19:12:12 +0200

    Add navigation link for projects

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -101,7 +101,7 @@
 				</svg>
 				<span class="font-semibold text-sm">{{ __('Invoices') }}</span>
 			</x-nav-link>
-            <x-nav-link :href="'#'" :active="false">
+            <x-nav-link :href="route('projects.index')" :active="request()->is('projects*')">
                 <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor">
 					<g>
 						<rect fill="none" height="24" width="24"/>
