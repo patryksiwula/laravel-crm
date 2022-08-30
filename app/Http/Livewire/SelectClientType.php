213cdcbcc2c313382118051ba5d25799519c29db
Patryk Siwula <patryk.siw@gmail.com>
2022-08-30 19:13:13 +0200

    Add select client type component

---
+++ app/Http/Livewire/SelectClientType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SelectClientType extends Component
+{
+	public array $clientTypes;
+	public string $clientType;
+
+	public function mount(): void
+	{
+		$this->clientType = 'Organization';
+	}
+
+    public function render()
+    {
+        return view('livewire.select-client-type');
+    }
+
+	public function setClientType(): void
+	{
+		$this->emit('clientTypeSelected', $this->clientType);
+	}
+}
