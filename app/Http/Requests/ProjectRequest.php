da5ad64b9d8561e633ca7b15a2e9c97085e26b38
Patryk Siwula <patryk.siw@gmail.com>
2022-08-30 19:13:59 +0200

    Add form request for projects

---
+++ app/Http/Requests/ProjectRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string'],
+			'description' => ['string', 'nullable'],
+			'deadline' => ['required', 'date'],
+			'user_id' => ['required', 'numeric', 'min:1'],
+			'model_id' => ['required', 'numeric', 'min:1'],
+			'model_type' => ['required', 'string', 'in:App\Models\Client\Organization,App\Models\Client\Person']
+        ];
+    }
+}
