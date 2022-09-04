3ba838c31f279ae79f43a315eff5414a0a149c9e
Patryk Siwula <patryk.siw@gmail.com>
2022-09-04 22:09:55 +0200

    Add meetings link to navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -121,7 +121,7 @@
 				</svg>
                 <span class="font-semibold text-sm">{{ __('Tasks') }}</span>
             </x-nav-link>
-			<x-nav-link :href="'#'" :active="false">
+			<x-nav-link :href="route('meetings.index')" :active="request()->is('meetings*')">
                 <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF">
 					<g>
 						<rect fill="none" height="24" width="24"/>
