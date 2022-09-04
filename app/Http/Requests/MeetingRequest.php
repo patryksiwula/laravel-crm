b938bef53fcc7c627119444651140a40e8d9f1ce
Patryk Siwula <patryk.siw@gmail.com>
2022-09-04 22:42:19 +0200

    Add form request for meetings

---
+++ app/Http/Requests/MeetingRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MeetingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'description' => ['required', 'string'],
+			'time' => ['required', 'datetime'],
+			'search.*.model_id' => ['required', 'numeric', 'min:1'],
+			'search.1.model_type' => ['required', 'string', 'in:App\Models\Client\Organization,App\Models\Client\Person']
+        ];
+    }
+}
