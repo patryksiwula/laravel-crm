f229fa4417ebb5a27db4193e1616aec39b18cb02
Patryk Siwula <patryk.siw@gmail.com>
2022-09-04 21:52:40 +0200

    Add meetings migration

---
+++ database/migrations/2022_09_04_193938_create_meetings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meetings', function (Blueprint $table) {
+            $table->id();
+			$table->string('description');
+			$table->dateTime('time');
+
+			$table->foreignId('user_id')->constrained()
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+
+			$table->morphs('client');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meetings');
+    }
+};
