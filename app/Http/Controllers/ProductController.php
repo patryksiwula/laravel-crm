b4014e18f7b9718fc665ab0cae98ad82a08817ea
Patryk Siwula <patryk.siw@gmail.com>
2022-09-06 01:22:50 +0200

    Add pagination for products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
      */
     public function index(): View
     {
-        $products = Product::all();
+        $products = Product::paginate(15);
 
 		return view('products.list', compact('products'));
     }
