ec21653e367b546da26c20c94500a0626a0ce16f
Patryk Siwula <patryk.siw@gmail.com>
2022-09-05 12:55:38 +0200

    Add document migration

---
+++ database/migrations/2022_09_05_082324_create_documents_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+			$table->string('file_name');
+			$table->string('extension');
+			$table->string('path');
+			$table->string('description');
+
+			$table->foreignId('user_id')->nullable()
+				->constrained()
+				->cascadeOnUpdate()
+				->nullOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+};
