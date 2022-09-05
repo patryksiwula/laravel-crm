038a02f653b4b4d8828443da89824dbf2717336f
Patryk Siwula <patryk.siw@gmail.com>
2022-09-05 10:31:46 +0200

    Add templates for configuration list and edit

---
+++ resources/views/configs/edit.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="min-h-screen w-full ml-60 bg-gray-200">
+        <div class="w-full py-5 text-center bg-white shadow-sm">
+            <h1 class="text-xl font-bold">{{ __('Updating configuration') }}</h1>
+        </div>
+
+        <div class="w-full h-full px-80 py-10 bg-gray-200">
+            <div class="w-full bg-white p-8 shadow-md">
+                <form action="{{ route('configs.update') }}" method="POST">
+                    @csrf
+					@method('PATCH')
+
+                    <div class="block">
+                        <label for="name" class="font-bold text-base text-black block mb-3">
+                            {{ __('Company name') }}
+                        </label>
+                        <input type="text" placeholder="{{ __('Company name') }}" name="name" value="{{ $configs->get(0)->value }}" class="w-full border-[1.5px] border-form-stroke
+                            rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
+                            active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
+                    </div>
+
+                    <div class="mt-8">
+                        <label for="email" class="font-bold text-base text-black block mb-3">
+                            {{ __('Email address') }}
+                        </label>
+                        <input type="email" placeholder="{{ __('Email address') }}" name="email" value="{{ $configs->get(1)->value }}" class="w-full border-[1.5px]
+							border-form-stroke rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
+                            active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
+                    </div>
+
+					<div class="mt-8">
+                        <label for="address" class="font-bold text-base text-black block mb-3">
+                            {{ __('Address') }}
+                        </label>
+                        <input type="text" name="address" placeholder="{{ __('Company address') }}" value="{{ $configs->get(2)->value }}" class="w-full border-[1.5px] border-form-stroke
+                            rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
+                            active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
+                    </div>
+
+					<div class="mt-8">
+                        <label for="vat" class="font-bold text-base text-black block mb-3">
+                            {{ __('VAT number') }}
+                        </label>
+                        <input type="text" name="vat" placeholder="{{ __('Company VAT number') }}" value="{{ $configs->get(3)->value }}" class="w-full border-[1.5px] border-form-stroke
+                            rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
+                            active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
+                    </div>
+
+					<div class="mt-8">
+						<label for="date_format" class="font-bold text-base text-black block mb-3">
+							{{ __('Date format') }}
+						</label>
+						<select name="date_format" placeholder="{{ __('Date display format') }}" class="w-full border-[1.5px] border-form-stroke
+                            rounded-lg py-3 px-5 font-medium text-body-color placeholder-body-color outline-none focus:border-primary
+                            active:border-primary transition disabled:bg-[#F5F7FD] disabled:cursor-default">
+							<option value="d.m.Y" @selected($configs->get(4)->value === 'd.m.Y')>{{ __('d.m.Y') }}</option>
+							<option value="d-m-Y" @selected($configs->get(4)->value === 'd-m-Y')>{{ __('d-m-Y') }}</option>
+							<option value="d/m/Y" @selected($configs->get(4)->value === 'd/m/Y')>{{ __('d/m/Y') }}</option>
+							<option value="Y.m.d" @selected($configs->get(4)->value === 'Y.m.d')>{{ __('Y.m.d') }}</option>
+							<option value="Y-m-d" @selected($configs->get(4)->value === 'Y-m-d')>{{ __('Y-m-d') }}</option>
+							<option value="Y/m/d" @selected($configs->get(4)->value === 'Y/m/d')>{{ __('Y/m/d') }}</option>
+						</select>
+					</div>
+
+                    <input type="submit" class="mt-10 text-lg bg-green-500 hover:bg-green-700 text-white py-2 px-6 rounded
+                    	focus:outline-none focus:shadow-outline font-bold cursor-pointer">
+                </form>
+            </div>
+        </div>
+    </main>
+@endsection
---
+++ resources/views/configs/list.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="min-h-screen w-full ml-60 bg-gray-200">
+        <div class="w-full py-5 text-center bg-white shadow-sm">
+            <h1 class="text-xl font-bold">{{ __('CRM Configuration') }}</h1>
+        </div>
+
+        <div class="flex flex-col w-full h-full px-10 2xl:px-60 pt-12 pb-10 bg-gray-200">
+			@can('edit-configs')
+				<div class="mb-6">
+					<a href="{{ route('configs.edit') }}" class="text-xl bg-blue-500 hover:bg-blue-700 text-white py-3 px-6 rounded
+						focus:outline-none focus:shadow-outline font-bold">
+						{{ __('Edit configuration') }}
+					</a>
+				</div>
+			@endcan
+
+            <div class="w-full bg-white">
+                <div class="text-gray-900 bg-gray-200">
+                    <div class="flex justify-center">
+                        <table class="w-full text-md bg-white shadow-md rounded mb-4">
+                            <tbody>
+                                <tr class="border-b">
+                                    <th class="text-center p-3 px-5">{{ __('Config') }}</th>
+									<th class="text-center p-3 px-5">{{ __('Value') }}</th>
+                                </tr>
+                                
+								<tr class="border-b hover:bg-orange-100 bg-gray-100">
+									<td class="text-center p-3 px-5">{{ __('Company name') }}</td>
+									<td class="text-center p-3 px-5">{{ $configs->get(0)->value }}</td>
+								</tr>
+
+								<tr class="border-b hover:bg-orange-100 bg-gray-100">
+									<td class="text-center p-3 px-5">{{ __('Email address') }}</td>
+									<td class="text-center p-3 px-5">{{ $configs->get(1)->value }}</td>
+								</tr>
+
+								<tr class="border-b hover:bg-orange-100 bg-gray-100">
+									<td class="text-center p-3 px-5">{{ __('Company address') }}</td>
+									<td class="text-center p-3 px-5">{{ $configs->get(2)->value }}</td>
+								</tr>
+
+								<tr class="border-b hover:bg-orange-100 bg-gray-100">
+									<td class="text-center p-3 px-5">{{ __('VAT number') }}</td>
+									<td class="text-center p-3 px-5">{{ $configs->get(3)->value }}</td>
+								</tr>
+
+								<tr class="border-b hover:bg-orange-100 bg-gray-100">
+									<td class="text-center p-3 px-5">{{ __('Date display format') }}</td>
+									<td class="text-center p-3 px-5">{{ $configs->get(4)->value }}</td>
+								</tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
