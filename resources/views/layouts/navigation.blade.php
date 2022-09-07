b6a329503f9f346a1babe5b143073a78856450d1
Patryk Siwula <patryk.siw@gmail.com>
2022-09-07 19:14:33 +0200

    Add lead list link to navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -55,7 +55,7 @@
 						<path d="M0 0h24v24H0V0z" fill="none"/>
 						<path d="M12 7V5c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2h-8zM6 19H4v-2h2v2zm0-4H4v-2h2v2zm0-4H4V9h2v2zm0-4H4V5h2v2zm4 12H8v-2h2v2zm0-4H8v-2h2v2zm0-4H8V9h2v2zm0-4H8V5h2v2zm9 12h-7v-2h2v-2h-2v-2h2v-2h-2V9h7c.55 0 1 .45 1 1v8c0 .55-.45 1-1 1zm-1-8h-2v2h2v-2zm0 4h-2v2h2v-2z"/>
 					</svg>
-					<span class="font-semibold text-sm">{{ __('Organizations') }}</span>
+					<span class="font-semibold text-sm">{{ __('Organisations') }}</span>
 				</x-nav-link-child>
 				<x-nav-link-child :href="route('people.index')" :active="request()->is('people*')">
 					<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor">
@@ -67,7 +67,7 @@
 					<span class="font-semibold text-sm">{{ __('People') }}</span>
 				</x-nav-link-child>
 			</ul>
-			<x-nav-link :href="'#'" :active="false">
+			<x-nav-link :href="route('leads.index')" :active="request()->is('leads*')">
 				<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF">
 					<g>
 						<path d="M0,0h24 M24,24H0" fill="none"/>
