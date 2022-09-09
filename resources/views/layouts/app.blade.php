7c64e58367b209cff1637a4eb8d02ae3cbd3d2a0
Patryk Siwula <patryk.siw@gmail.com>
2022-09-09 16:03:27 +0200

    Install Livewire Charts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,9 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 		@livewireScripts
+		@livewireChartsScripts
 		<script src="{{ asset('bladewind/js/helpers.js') }}"></script>
+		<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
